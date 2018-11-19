cc432545fa238a9fe356cdc12dff3c30b1a9f209
Sahar Brodbeker <saharonb@gmail.com>
2018-11-19 17:33:44 +0200

    Add functionality (#1)

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # transform-keys [![](https://img.shields.io/npm/v/@cocopina/transform-keys.svg?colorA=cb3837&colorB=474a50)](https://www.npmjs.com/package/@cocopina/transform-keys)
 
-Deeply mutate an object&#39;s keys
+Creates a deep clone of a JSON data structure with keys transformed by a provided function.
 
 ## Table of Contents
 
 - [Installation](#installation)
 - [API](#api)
+    * [`transformKeys(object, transform, [options])`](#transformkeysobject-transform-options)
+        * [`object [Object]`](#object-object)
+        * [`transform [Function]`](#transform-function)
+        * [`options [Object]`](#options-object)
 
 ## Installation
 
@@ -15,3 +19,36 @@ npm i @cocopina/transform-keys
 ```
 
 ## API
+
+### `transformKeys(object, transform, [options])`
+
+Returns a new, transformed object.
+
+```js
+import transformKeys from '@cocopina/transform-keys';
+
+const transformFn = (value) => `_${value}`;
+const object = {
+    key: 1
+};
+const transformed = transformKeys(object, transformFn);
+
+console.log(transformed); // {_key: 1}
+```
+
+#### `object [Object]`
+
+The object which keys' you wish to transform.
+
+#### `transform [Function]`
+
+The transformation function you wish to use on the object's keys.
+
+#### `options [Object]`
+
+Additional options:
+
+| Option | Type | What It Does | Default |
+|------------------|--------------------|-----|-----|
+| `deep` | `Boolean` | Whether or not to transform the object's keys recursively | `true` |
+| `recursionLevel` | `Number` | How deep should the transformation go | - |
--- spec.js
+++ spec.js
@@ -1,7 +1,89 @@
-import package from './index';
+import _ from 'lodash';
+import transformKeys from '.';
+import { NO_TRANSFORMATION_ERROR_MESSAGE } from './constants';
 
-describe('package', () => {
-    it('Packs a punch!', () => {
-        expect(true).toEqual(true);
+const TRANSFORM_FUNCTION = _.camelCase;
+
+describe('transformKeys', () => {
+    it('Transforms a given object\'s keys, regardless of the case convention used', () => {
+        const nonCamlizedObject = {
+            snake_key: 1,
+            PascalKey: 1,
+            'kebab-key': 1,
+            '__JuSt_-_weIrd': 1,
+        };
+        const transformedObject = transformKeys(nonCamlizedObject, TRANSFORM_FUNCTION);
+        const expectedObject = {
+            snakeKey: 1,
+            pascalKey: 1,
+            kebabKey: 1,
+            juStWeIrd: 1,
+        };
+
+        expect(transformedObject).toEqual(expectedObject);
+    });
+
+    it('Generates a new object and not a pointer', () => {
+        const object = {
+            firstLevel: {
+                secondLevel: 1,
+            },
+        };
+        const transformedObject = transformKeys(object, TRANSFORM_FUNCTION);
+
+        expect(transformedObject.firstLevel).not.toBe(object.firstLevel);
+    });
+
+    it('Transforms keys recursively indefinitely', () => {
+        const object = {
+            first_level: {
+                second_level: 1,
+            },
+        };
+        const expectedObject = {
+            firstLevel: {
+                secondLevel: 1,
+            },
+        };
+        const transformedObject = transformKeys(object, TRANSFORM_FUNCTION, { deep: true });
+
+        expect(transformedObject).toEqual(expectedObject);
+    });
+
+    it('Transforms keys recursively, to a certain level', () => {
+        const object = {
+            first_level: {
+                second_level: 1,
+            },
+        };
+        const expectedObject = {
+            firstLevel: {
+                second_level: 1,
+            },
+        };
+        const transformedObject = transformKeys(object, TRANSFORM_FUNCTION, { deep: true, recursionLevel: 1 });
+
+        expect(transformedObject).toEqual(expectedObject);
+    });
+
+    it('Throws an error if a transformation function wasn\'t passed', () => {
+        const badUsage = () => transformKeys({});
+
+        expect(badUsage).toThrowError(NO_TRANSFORMATION_ERROR_MESSAGE);
+    });
+
+    it('Throws an error if not passed a function as the second argument', () => {
+        const badValues = [
+            1,
+            'string',
+            [],
+            {},
+        ];
+
+        badValues.forEach((value) => {
+            const badUsage = () => transformKeys({}, value);
+
+            expect(badUsage).toThrowError(NO_TRANSFORMATION_ERROR_MESSAGE);
+        });
     });
 });
